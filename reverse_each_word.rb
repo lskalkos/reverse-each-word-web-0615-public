74d137063e1e63e58c8e033d6673b02618cd11a2
Lia Skalkos <hello@liaskalkos.net>
2015-06-04 15:54:03 -0400

    Reverse each way of making it work

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,13 +5,7 @@ def reverse_each_word(sentence)
   array.each do |word|
     letters = word.split("")
     reverse_word = []
-    i = letters.length
-
-    while i >= 0
-      reverse_word << word[i]
-      i-=1
-    end
-
+    letters.reverse_each{|letter| reverse_word << letter}
     reverse_array << reverse_word.join("")
   end
 
